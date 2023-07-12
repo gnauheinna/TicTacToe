c7c92b3e7bc291c550f7b0cac9c723acebb76559
gnauheinna <116969903+gnauheinna@users.noreply.github.com>
2023-07-12 14:00:42 +0800

    updated qs qsAll menutoggle and turn handler

--- js/view.js
+++ js/view.js
@@ -1,25 +1,34 @@
 export default class View {
 
     $ = {}
+    $$ = {}
 
 
     constructor() {
-        this.$.menu = document.querySelector('[data-id="menu"]');
-        this.$.menuBtn = document.querySelector('[data-id="menu-btn"]');
-        this.$.menuItems = document.querySelector('[data-id="menu-items"]');
-        this.$.resetBtn = document.querySelector('[data-id="reset-btn"]');
-        this.$.newRoundBtn = document.querySelector('[data-id="new-round-btn"]');
-        this.$.squares = document.querySelectorAll('[data-id="square"]');
-        this.$.modal = document.querySelector('[data-id="modal"]');
-        this.$.modalText = document.querySelector('[data-id="modal-text"]');
-        this.$.modalBtn = document.querySelector('[data-id="modal-btn"]');
-        this.$.turn = document.querySelector('[data-id="turn"]');
+        this.$.menu = this.#qs('[data-id="menu"]');
+        this.$.menuBtn = this.#qs('[data-id="menu-btn"]');
+        this.$.menuItems = this.#qs('[data-id="menu-items"]');
+        this.$.resetBtn = this.#qs('[data-id="reset-btn"]');
+        this.$.newRoundBtn = this.#qs('[data-id="new-round-btn"]');
+        this.$.modal = this.#qs('[data-id="modal"]');
+        this.$.modalText = this.#qs('[data-id="modal-text"]');
+        this.$.modalBtn = this.#qs('[data-id="modal-btn"]');
+        this.$.turn = this.#qs('[data-id="turn"]');
+
+        this.$$.squares = this.#qsAll('[data-id="square"]');
+
 
         //UI-only Event Listners
+        // menu toggling
         this.$.menuBtn.addEventListener("click", (event) => {
-            this.$.menuItems.classList.toggle("hidden");
+            this.#toggleMenu();
         });
     }
+
+    /*
+     *   Register all the event listeners
+     */
+
  
     bindGameResetEvent(handler) {
         this.$.resetBtn.addEventListener('click', handler);
@@ -30,8 +39,68 @@ export default class View {
     }
 
     bindPlayerMoveEvent(handler) {
-        this.$.squares.forEach((square) => {
+        this.$$.squares.forEach((square) => {
             square.addEventListener('click', handler);
         });
+    }
+
+
+
+    /*
+     *  DOM helper methods
+     */
+
+    //menu toggling helper method
+    #toggleMenu() {
+        this.$.menuItems.classList.toggle("hidden");
+        this.$.menuBtn.classList.toggle('border');
+
+        const icon = this.$.menuBtn.querySelector('i');
+        icon.classList.toggle('fa-chevron-down');
+        icon.classList.toggle('fa-chevron-up');
+    }
+
+    handlePlayerMove(squareEl, player) {
+        const icon = document.createElement('i');
+        icon.classList.add('fa-solid',
+        player === 1 ? 'fa-x' : 'fa-o',
+        player === 1 ? 'yellow' : 'turquoise')
+        squareEl.replaceChildren(icon);
+    }
+
+    // player = 1 | 2
+    setTurnIndicator(player){ 
+        const icon = document.createElement('i')
+        const label = document.createElement('p')
+
+        // adds/ removes correct color for player
+        this.$.turn.classList.add(player === 1 ? 'yellow' : 'turquoise')
+        this.$.turn.classList.remove(player === 1 ? 'turquiose' : 'yellow')
+
+        // adds correct icon for player
+
+        icon.classList.add('fa-solid', player === 1? 'fa-x': 'fa-o')
+
+        //label
+        label.innerText = 
+            player === 1? "Player 1, you're up!" : "Player 2, youre're up!"
+
+        this.$.turn.replaceChildren(icon, label);
+    }
+
+    // private query selector + checker helper method 
+    #qs(selector, parent) {
+        const el = parent? parent.querySelector(selector): 
+        document.querySelector(selector);
+        if(!el) throw new Error("couldn't find selector");
+
+        return el;
+    }
+
+    #qsAll(selector) {
+        const ellist = document.querySelectorAll(selector);
+        if(!ellist) throw new Error("couldn't find selector");
+
+        return ellist;
     }
 }
